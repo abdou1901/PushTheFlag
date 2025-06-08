8cfb62ca0c2127f79fda88a89cd655749b2ca712
abdou1901 <110035060+abdou1901@users.noreply.github.com>
2025-06-08 10:14:09 +0100

    Update the list of valid languages

--- src/services/CodeforcesApiDoc.mjs
+++ src/services/CodeforcesApiDoc.mjs
@@ -32,43 +32,8 @@ function isValidUrl(RawUrl) {
 function isValidLanguage(language){
   console.log("language : ",language)
     const validLanguages = [
-        "GNU GCC C11 5.1.0",
-        "GNU G++17 7.3.0",
-        "GNU G++20 13.2 (64 bit, winlibs)",
-        "GNU G++23 14.2 (64 bit, msys2)",
-        "C# 8, .NET Core 3.1",
-        "C# 10, .NET SDK 6.0",
-        "C# 13, .NET SDK 9",
-        "C# Mono 6.8",
-        "D DMD32 v2.105.0",
-        "F# 9, .NET SDK 9",
-        "Go 1.22.2",
-        "Haskell GHC 8.10.1",
-        "Java 21 64bit",
-        "Java 8 32bit",
-        "Kotlin 1.7.20",
-        "Kotlin 1.9.21",
-        "OCaml 4.02.1",
-        "Delphi 7",
-        "Free Pascal 3.2.2",
-        "PascalABC.NET 3.8.3",
-        "Perl 5.20.1",
-        "PHP 8.1.7",
-        "Python 2.7.18",
-        "python3",
-        "python",
-        "Python",
-        "c",
-        "C",
-        "Python 3.13.2",
-        "PyPy 2.7.13 (7.3.0)",
-        "PyPy 3.6.9 (7.3.0)",
-        "PyPy 3.10 (7.3.15, 64bit)",
-        "Ruby 3.2.2",
-        "Rust 1.75.0 (2021)",
-        "Scala 2.12.8",
-        "JavaScript V8 4.8.0",
-        "Node.js 15.8.0 (64bit)"
+        "python","python3", "c","c#" , "c++", "java", "javascript", 
+        "typescript", "php","swift","go","ruby","rust","kotlin","dart"
     ];
     return validLanguages.includes(language)
 }
