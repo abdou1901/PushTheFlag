805ed614b15195f2bbf9d6429f8d0f92dbd0b3a7
abdou1901 <110035060+abdou1901@users.noreply.github.com>
2025-06-09 18:23:43 +0100

    Delete prisma/migrations directory

--- prisma/migrations/20250601125124_extend_the_schema/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `solution` on the `SolvedCodeforces` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "SolvedCodeforces" DROP COLUMN "solution";
--- prisma/migrations/20250603113706_extend_the_schema/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "branch_name" TEXT NOT NULL DEFAULT 'main';
