dc2f2c33e92f9b9442fefefdb78929370b4496c2
abdou1901 <110035060+abdou1901@users.noreply.github.com>
2025-06-10 12:03:45 +0100

    Delete prisma/20250610100211_fix directory

--- prisma/20250610100211_fix/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "codeforces_repo" SET DEFAULT 'codeforces',
-ALTER COLUMN "ctf_repo" SET DEFAULT 'ctf_writeups',
-ALTER COLUMN "leetcode_repo" SET DEFAULT 'leetcode';
